0617a9c51bdd9e2774fc6370644dbdfae6050a72
Enrico Meinel <emeinel@valuephone.com>
2017-04-25 08:41:23 +0200

    added debug output

--- work/experimental.sh
+++ work/experimental.sh
@@ -40,11 +40,11 @@ fi
 # build
 git clone https://github.com/FreifunkVogtland/gluon.git "${GLUONDIR}" -b v"${GLUON_VERSION}"
 git clone https://github.com/FreifunkVogtland/site-ffv.git "${GLUONDIR}"/site -b "${SITE_TAG}"
-make -C "${GLUONDIR}" update
+make -C "${GLUONDIR}" update V=s
 for target in ${TARGETS}; do
-    make -C "${GLUONDIR}" GLUON_TARGET="${target}" BROKEN=1 clean -j"$(nproc || echo 1)"
-    make -C "${GLUONDIR}" GLUON_TARGET="${target}" BROKEN=1 GLUON_BRANCH="${TARGET_BRANCH}" -j"$(nproc || echo 1)"
+    make -C "${GLUONDIR}" GLUON_TARGET="${target}" BROKEN=1 clean -j"$(nproc || echo 1)" V=s
+    make -C "${GLUONDIR}" GLUON_TARGET="${target}" BROKEN=1 GLUON_BRANCH="${TARGET_BRANCH}" -j"$(nproc || echo 1)"  V=s
 done
 
-make -C "${GLUONDIR}" GLUON_BRANCH="${TARGET_BRANCH}" BROKEN=1 manifest
+make -C "${GLUONDIR}" GLUON_BRANCH="${TARGET_BRANCH}" BROKEN=1 manifest V=s
 "${GLUONDIR}"/contrib/sign.sh "${SIGN_KEYDIR}/${MANIFEST_KEY}" "${GLUONDIR}"/output/images/sysupgrade/"${TARGET_BRANCH}".manifest
