f91185c34a6e8235c1047d74fae51b742fb4c16f
Enrico Meinel <altima@users.noreply.github.com>
2018-07-22 22:31:15 +0200

    Update single.sh

--- build/single.sh
+++ build/single.sh
@@ -8,4 +8,4 @@ make -C "${GLUONDIR}" update
 make -C "${GLUONDIR}" GLUON_TARGET=${GLUON_SINGLE_TARGET} DEVICES="${GLUON_SINGLE_DEVICE}" GLUON_BRANCH="${TARGET_BRANCH}" -j"$(nproc || echo 1)"
 
 make -C "${GLUONDIR}" GLUON_BRANCH="${TARGET_BRANCH}" BROKEN=1 manifest
-"${GLUONDIR}"/contrib/sign.sh "${SIGN_KEYDIR}/${MANIFEST_KEY}" "${GLUONDIR}"/output/images/sysupgrade/"${TARGET_BRANCH}".manifest
+#"${GLUONDIR}"/contrib/sign.sh "${SIGN_KEYDIR}/${MANIFEST_KEY}" "${GLUONDIR}"/output/images/sysupgrade/"${TARGET_BRANCH}".manifest
