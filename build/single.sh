585a214d7933beeb59a7d6d2465cee81fba77ebb
Enrico Meinel <altima@users.noreply.github.com>
2018-07-22 22:41:04 +0200

    Update single.sh

--- build/single.sh
+++ build/single.sh
@@ -5,7 +5,7 @@ git clone https://github.com/FreifunkVogtland/gluon.git "${GLUONDIR}" -b v"${GLU
 git clone https://github.com/FreifunkVogtland/site-ffv.git "${GLUONDIR}"/site -b "${SITE_TAG}"
 make -C "${GLUONDIR}" update
 
-make -C "${GLUONDIR}" GLUON_TARGET=${GLUON_SINGLE_TARGET} DEVICES="${GLUON_SINGLE_DEVICE}" GLUON_BRANCH="${TARGET_BRANCH}" -j"$(nproc || echo 1)"
+make -C "${GLUONDIR}" GLUON_TARGET=${GLUON_SINGLE_TARGET} DEVICES="${GLUON_SINGLE_DEVICE}" GLUON_BRANCH="${TARGET_BRANCH}" -j"$(nproc || echo 1)" V=s
 
-make -C "${GLUONDIR}" GLUON_BRANCH="${TARGET_BRANCH}" BROKEN=1 manifest
+make -C "${GLUONDIR}" GLUON_BRANCH="${TARGET_BRANCH}" BROKEN=1 V=s manifest
 #"${GLUONDIR}"/contrib/sign.sh "${SIGN_KEYDIR}/${MANIFEST_KEY}" "${GLUONDIR}"/output/images/sysupgrade/"${TARGET_BRANCH}".manifest
